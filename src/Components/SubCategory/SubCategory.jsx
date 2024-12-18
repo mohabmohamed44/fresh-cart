da820d94f8af0bc382a8092a122042676445402e
Mohab Mohammed <92528415+mohabmohamed44@users.noreply.github.com>
2024-12-18 19:50:54 +0200

    Initial Commit..

--- src/Components/SubCategory/SubCategory.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect} from "react";
-import Style from "./SubCategory.module.css";
-export default function SubCategory() {
-  return (
-    <>
-     <h2>Template Name</h2>
-    </>
-  );
-}
